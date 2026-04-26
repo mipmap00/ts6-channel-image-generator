f915eb9baf499ec4d12f122e789aa3d6d9f9392a
mipmap00 <mipmap00@users.noreply.github.com>
2026-04-26 02:52:17 +0000

    deploy: e07a9f5b4fac6bd215f4ae679859e182389f605a

---
+++ _next/static/3LU5x2RtwFGLzxiT-WfN6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/a3158098b2527b3c.css","static/chunks/455-550181762086022c.js","static/chunks/pages/index-9959848999f36756.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
